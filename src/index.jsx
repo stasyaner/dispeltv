d559ca024cc4907868aa67f712eb1f9cd2291933
Stanislav Sidletskiy <stasyaner@hotmail.com>
2017-08-03 13:04:34 +0200

    hot-reload with appcontainer

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import Root from './containers/Root';
 import configureStore from './store';
 
 render(
-  <Root store={configureStore()} />,
+  <AppContainer>
+    <Root store={configureStore()} />
+  </AppContainer>,
   document.getElementById('root'),
 );
+
+if (module.hot) {
+  module.hot.accept('./containers/Root', () => { render(<Root store={configureStore()} />); });
+}
