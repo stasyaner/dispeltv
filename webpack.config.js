fe76b595d43e0e2bde98ea389fc4f8ddaddae520
Stanislav Sidletskiy <stasyaner@hotmail.com>
2017-08-03 13:03:09 +0200

    Sass loading, dev-server config and html-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+let config = {
   entry: {
-    index: path.join(__dirname, './src/index.jsx'),
-    vendor: ['react', 'react-dom'],
+    index: ['react-hot-loader/patch', path.join(__dirname, './src/index.jsx')],
+    vendor: [
+      'react',
+      'react-dom',
+      'react-redux',
+      'react-router',
+      'react-router-dom',
+      'redux-thunk',
+      'redux',
+      'react-hot-loader',
+      'firebase',
+      'hls.js'
+    ],
   },
   output: {
      path: path.join(__dirname, './public'),
-     filename: '[name].bundle.js',
+     filename: '[name].bundle-[hash].js',
   },
   module: {
     rules: [
@@ -18,30 +29,55 @@ module.exports = {
         exclude: /node_modules/,
         use: [{
           loader: 'babel-loader',
-          options: { presets: ['es2015', 'react'] },
+          options: {
+            presets: [['es2015', { 'modules': false }], 'react'],
+            plugins: ['react-hot-loader/babel']
+          },
         }],
       },
       {
-        test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader']
-      }
-      ,
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000&minetype=application/font-woff'
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'file-loader'
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          'sass-loader'
+        ]
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.less'],
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
     }),
+    new HtmlWebpackPlugin({
+      template: 'template.html',
+      inject: 'body',
+      hash: true,
+    }),
   ],
  };
+
+ if (process.env.NODE_ENV === 'production') {
+  config.plugins = [
+    new webpack.optimize.UglifyJsPlugin({
+      warnings: false,
+      screw_ie8: true,
+      drop_console: true,
+      drop_debugger: true,
+      dead_code: true,
+      comments: false,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify('production') },
+    })
+  ];
+};
+
+module.exports = config;
