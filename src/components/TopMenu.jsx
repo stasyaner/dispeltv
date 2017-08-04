cbd544dcee328dc3d86e1436a37d422a12806943
Stanislav Sidletskiy <stasyaner@hotmail.com>
2017-08-04 15:35:22 +0200

    from test html/css to more realistic

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import topmenu from '../css/TopMenu.scss';
+import { Link } from 'react-router-dom';
+import style from '../css/TopMenu.scss';
 
 const TopMenu = () => (
-  <ul className="top-menu">
-    <li><span className={topmenu.logo}>DispelTV</span></li>
-    <li>Main</li>
-  </ul>
+  <div className={style.topMenu}>
+    <ul>
+      <li className={style.logo}><Link to="/">DispelTV</Link></li>
+      <li><Link to="/">main</Link></li>
+      <li><Link to="/">test</Link></li>
+    </ul>
+  </div>
 );
 
 export default TopMenu;
