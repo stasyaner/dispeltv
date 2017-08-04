88dac8420c06528119d4e55adb53cb1b439650a5
Stanislav Sidletskiy <stasyaner@hotmail.com>
2017-08-04 18:36:41 +0200

    defining font faces for different styles of Open Sans

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,10 @@ import { AppContainer } from 'react-hot-loader';
 import Root from './containers/Root';
 import configureStore from './store';
 import './css/Global.scss';
-import './css/fonts/OpenSans-Regular.ttf';
+import './css/fonts/OpenSans.woff2';
+import './css/fonts/OpenSans-Bold.woff2';
+import './css/fonts/OpenSans-Italic.woff2';
+import './css/fonts/OpenSans-BoldItalic.woff2';
 
 render(
   <AppContainer>
