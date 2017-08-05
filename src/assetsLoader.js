f3d4eb201ce452e04008416b7d13bf7f71ad3ed5
Stanislav Sidletskiy <stasyaner@hotmail.com>
2017-08-05 11:22:06 +0200

    assets imports into the different file

---
+++ src/assetsLoader.js
@@ -0,0 +1,6 @@
+import './css/Global.scss';
+import './css/fonts/OpenSans.woff2';
+import './css/fonts/OpenSans-Bold.woff2';
+import './css/fonts/OpenSans-Italic.woff2';
+import './css/fonts/OpenSans-BoldItalic.woff2';
+import './css/icons/two-circles.svg';
--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,7 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from './containers/Root';
 import configureStore from './store';
-import './css/Global.scss';
-import './css/fonts/OpenSans.woff2';
-import './css/fonts/OpenSans-Bold.woff2';
-import './css/fonts/OpenSans-Italic.woff2';
-import './css/fonts/OpenSans-BoldItalic.woff2';
+import './assetsLoader';
 
 render(
   <AppContainer>
