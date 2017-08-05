60847e62673cf44b6d1c27afa279fef61ee575ac
Stanislav Sidletskiy <stasyaner@hotmail.com>
2017-08-05 11:20:52 +0200

    search form added

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -6,8 +6,19 @@ const TopMenu = () => (
   <div className={style.topMenu}>
     <ul>
       <li className={style.logo}><Link to="/">DispelTV</Link></li>
-      <li><Link to="/">main</Link></li>
-      <li><Link to="/">test</Link></li>
+      <li>
+        <form className={style.search}>
+          <input
+            className={style.searchInput}
+            type="text"
+            placeholder="search in diff lang"
+          />
+          <button
+            className={style.searchButton}
+            type="submit"
+          ><span className={style.glyphSearch12}>&nbsp;</span></button>
+        </form>
+      </li>
     </ul>
   </div>
 );
